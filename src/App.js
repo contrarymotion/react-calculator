266e9504ab407247adbb9167addc2d84bbb3adc5
Mark Noesen <noesen.mark@gmail.com>
2023-08-21 22:14:38 -0400

    Add input from buttons

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
+import { useState } from 'react';
+
 import './App.css';
 import Button from './components/Button';
 import Input from './components/Input';
 
 const App = () => {
+
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+
+  const addToText = (val) => {
+    setText((text) => [...text, val + " "]);
+  }
+
+  const resetInput = () => {
+    setText("")
+    setResult("")
+  }
+
+
   const buttonColor = "#f2a33c";
 
   return (
     <div className="App">
       <div className="calc-wrapper">
-        <Input/>
+        <Input text={text} result={result}/>
         <div className="row">
-          <Button symbol="7"/>
-          <Button symbol="8"/>
-          <Button symbol="9"/>
-          <Button symbol="/" color={buttonColor}/>
+          <Button symbol="7" handleClick={addToText}/>
+          <Button symbol="8" handleClick={addToText}/>
+          <Button symbol="9" handleClick={addToText}/>
+          <Button symbol="/" color={buttonColor} handleClick={addToText}/>
         </div>
         <div className="row">
-          <Button symbol="4"/>
-          <Button symbol="5"/>
-          <Button symbol="6"/>
-          <Button symbol="*"/>
+          <Button symbol="4" handleClick={addToText}/>
+          <Button symbol="5" handleClick={addToText}/>
+          <Button symbol="6" handleClick={addToText}/>
+          <Button symbol="*" color={buttonColor} handleClick={addToText}/>
         </div>  
         <div className="row">
-          <Button symbol="1"/>
-          <Button symbol="2"/>
-          <Button symbol="3"/>
-          <Button symbol="+"/>
+          <Button symbol="1" handleClick={addToText}/>
+          <Button symbol="2" handleClick={addToText}/>
+          <Button symbol="3" handleClick={addToText}/>
+          <Button symbol="+" color={buttonColor} handleClick={addToText}/>
         </div>  
         <div className="row">
-          <Button symbol="0"/>
-          <Button symbol="."/>
-          <Button symbol="="/>
-          <Button symbol="-"/>
-        </div>     
+          <Button symbol="0" handleClick={addToText}/>
+          <Button symbol="." handleClick={addToText}/>
+          <Button symbol="=" handleClick={addToText}/>
+          <Button symbol="-" color={buttonColor} handleClick={addToText}/>
+        </div> 
+        <Button symbol="Clear" color="red" handleClick={resetInput}/>   
       </div>
     </div>
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,8 @@ import "./Button.css";
 const Button = ({ symbol, color, handleClick }) => {
   return (
     <div
-      // onClick={() => handleClick(symbol)}
+
+      onClick={() => handleClick(symbol)}
       className="button-wrapper"
       style={{ backgroundColor: color }}
     >
